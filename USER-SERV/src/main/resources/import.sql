21b790248e25f985b533ca6797f3141a8602d54c
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-11-20 10:37:13 -0600

    import.sql

--- USER-SERV/src/main/resources/import.sql
+++ USER-SERV/src/main/resources/import.sql
@@ -1,3 +1,10 @@
--- INSERT INTO Book VALUES(NULL, 35.0, 'Robert C. Martin','978-0132350884', 'Clean Code: A Handbook of Agile Software Craftsmanship');
--- INSERT INTO Book VALUES(NULL, 40.25, 'Andrew Hunt and David Thomas','978-0135957059', 'The Pragmatic Programmer: Your Journey to Mastery');
--- INSERT INTO Book VALUES(NULL, 50.75, 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides', '978-0201633610',  'Design Patterns: Elements of Reusable Object-Oriented Software');
+INSERT INTO Role VALUES(NULL, 'Admin');
+INSERT INTO Role VALUES(NULL, 'User');
+
+INSERT INTO User VALUES(NULL, 'Alan', 'Smith', '$2a$10$SKww238GdhtdxsBXxbf7JeI2dSHC5IkiACM1yZtA1pwaD2puWQDN2', 'admin1@miu.edu');
+INSERT INTO User VALUES(NULL, 'Helen', 'Pearson', '$2a$10$SKww238GdhtdxsBXxbf7JeI2dSHC5IkiACM1yZtA1pwaD2puWQDN2', 'user1@miu.edu');
+INSERT INTO User VALUES(NULL, 'Robin', 'Plevin', '$2a$10$SKww238GdhtdxsBXxbf7JeI2dSHC5IkiACM1yZtA1pwaD2puWQDN2', 'user2@miu.edu');
+
+INSERT INTO User_Roles VALUES(1, 1);
+INSERT INTO User_Roles VALUES(2, 2);
+INSERT INTO User_Roles VALUES(2, 3);
